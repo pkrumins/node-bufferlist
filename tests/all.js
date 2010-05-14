fbc056b4d3877c3c4cb661c853577cf8e3dbe85e
James Halliday <substack@gmail.com>
2010-05-14 03:49:59 -0800

    async unit tests! \\m/

---
+++ tests/all.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var sys = require('sys');
+var path = require('path');
+var proc = require('child_process');
+
+var scriptName = path.basename(__filename);
+
+fs.readdir(__dirname, function (err, files) {
+    var tests = files.filter(function (file) {
+        return file.match(/\.js$/) && file != scriptName;
+    });
+    
+    var failed = 0;
+    var ok = 0;
+    
+    (function nextTest () {
+        var test = tests.pop();
+        if (!test) {
+            sys.puts(
+                (ok + failed) + ' tests: '
+                + ok + ' ok, ' + failed + ' failed'
+            );
+        }
+        else {
+            sys.print(test + ' ... ');
+            proc.exec(__dirname + '/' + test, function (err, stdout, stderr) {
+                if (err) {
+                    sys.puts('failed');
+                    sys.puts(err);
+                    failed ++;
+                }
+                else if (stderr) {
+                    sys.puts('failed');
+                    sys.puts(stderr);
+                    failed ++;
+                }
+                else {
+                    sys.puts('ok');
+                    ok ++;
+                }
+                nextTest();
+            });
+        }
+    })();
+});
