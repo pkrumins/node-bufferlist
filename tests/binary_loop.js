36bf84a9ba81c3a5a177ab63ff918bd134882e62
James Halliday <substack@gmail.com>
2010-05-23 04:13:38 -0800

    intensive binary loop test with forever and repeat

--- tests/binary_loop.js
+++ tests/binary_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Functional binary interface
+// Test binary looping functions
 var assert = require('assert');
 var sys = require('sys');
 
@@ -10,10 +10,9 @@ var Binary = require('bufferlist/binary').Binary;
 // test repeat
 var reps = 0;
 var trickyList = [];
-var bList = new BufferList;
 
-Binary(bList)
-    .repeat(5000, function(n, vars) {
+Binary(new BufferList)
+    .repeat(5000, function(vars, n) {
         reps++;
     })
     .tap(function (vars) {
@@ -68,3 +67,61 @@ setTimeout(function () {
         c, 10000, 'c != 100, c == ' + c + ' in terminated forever check'
     );
 }, 1000);
+
+var bufferList = new BufferList;
+var loops = 0;
+Binary(bufferList)
+    .forever(function () {
+        this
+            .getWord16be('count')
+            .tap(function (vars) {
+                assert.equal(
+                    vars.count, 100, 'count != 100, count == ' + vars.count
+                );
+                vars.reps = 0;
+            })
+            .repeat('count', function (vars, i) {
+                this
+                    .getWord16be('size')
+                    .tap(function (vars) {
+                        assert.equal(
+                            vars.size, 1000,
+                            'size != 1000, size == ' + vars.size
+                        );
+                    })
+                    .getBuffer('block', 'size')
+                ;
+                vars.reps ++;
+            })
+            .tap(function (vars) {
+                assert.equal(
+                    vars.reps, 100, 'reps != 100, reps == ' + vars.reps
+                );
+            })
+        ;
+        loops ++;
+        if (loops == 20) this.exit();
+    })
+;
+
+for (var n = 0; n < 20; n++) {
+    var countBuf = new Buffer(2);
+    countBuf[0] = Math.floor(100 / 256);
+    countBuf[1] = 100 % 256;
+    bufferList.push(countBuf);
+    
+    for (var i = 0; i < 100; i++) {
+        var buf = new Buffer(1000 + 2);
+        buf[0] = Math.floor(1000 / 256);
+        buf[1] = 1000 % 256;
+        for (var j = 0; j < 1000; j++) {
+            buf[j + 2] = j;
+        }
+        bufferList.push(buf);
+    }
+}
+
+setTimeout(function () {
+    assert.equal(loops, 20, 'loops != 20, loops == ' + loops);
+}, 1000);
+
